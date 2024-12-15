77b780d7f8cd4ddfa7a27dd2a432ff48095b74ba
Prashanta Deuja <prashantadeuja@gmail.com>
2024-12-15 21:05:17 +0545

    initialize app.js

---
+++ Backend/src/app.js
@@ -0,0 +1,33 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import fileUpload from "express-fileupload";
+import { errorMiddleware } from "./middlewares/error.js";
+import messageRoutes from "./routes/messageRoutes.js";
+
+
+const app=express();
+
+
+app.use(cors({
+    origin:[process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL],
+    methods:["GET", "POST", "DELETE", "PUT"],
+    credentials:true,
+})
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+})
+);
+app.use("/api/v1/message", messageRoutes);
+ 
+app.use(errorMiddleware);
+
+export default app;
+
+
