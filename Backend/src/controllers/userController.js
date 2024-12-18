ebf7f2cec847342dddd45a0d93ba1181f68a32c9
Prashanta Deuja <prashantadeuja@gmail.com>
2024-12-18 15:40:30 +0545

    fix: Fix Cloudinary Issue and Import User Schema in User Controller

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -1,12 +1,13 @@
+import { v2 as Cloudinary } from "cloudinary";
 import { catchAsyncError } from "../middlewares/catchAsyncError.js";
-import ErrorHandler from "../middlewares/error.js";
+
+import { User } from "../models/userSchema.js";
 
 export const register = catchAsyncError(async(req ,res , next)=>{
-    if (!req.files || Object.keys(req.files).length=== 0){
+    if ( Object.keys(req.files).length=== 0){
         return next (new ErrorHandler("Avatar and  Resume Are Required",400));
     }
-    const {avatar, resume } = req.files;
-
+    const {avatar} = req.files;
     const cloudinaryResponseForAvatar= await Cloudinary.uploader.upload(
     avatar.tempFilePath,
     {folder: "AVATARS"}
@@ -17,7 +18,7 @@ if(!cloudinaryResponseForAvatar||cloudinaryResponseForAvatar.error){
         cloudinaryResponseForAvatar.error|| "unknown cloudinary error"
         );
     }
-
+    const {resume} = req.files;
     const cloudinaryResponseForResume = Cloudinary.uploader.upload(
         resume.tempFilePath,
         {folder: "RESUME"}
@@ -40,31 +41,37 @@ const{
     linkedinURL,
     instagramURL} = req.body;
 
-    const user = await user.create({
-        fullName,
-        email,
-        phone,
-        aboutMe,
-        password,
-        portfolioURL,
-        githubURL,
-        facebookURL,
-        linkedinURL,
-        instagramURL,   
-        avatar:{
-            public_id:cloudinaryResponseForAvatar.public_id,
-            
-            url:cloudinaryResponseForAvatar.secure_url,
-        },
-        resume:{
-            public_id:cloudinaryResponseForAvatar.public_id,
-            
-            url:cloudinaryResponseForAvatar.secure_url,
-        },
-    });
-    res.status(200).json({
-        sucess:true,
-        message:"user registered",
-    });
+    console.log(req.body);
+    try {
+        const user = await User.create({
+            fullName,
+            email,
+            phone,
+            aboutMe,
+            password,
+            portfolioURL,
+            githubURL,
+            facebookURL,
+            linkedinURL,
+            instagramURL,   
+            avatar:{
+                public_id:cloudinaryResponseForAvatar.public_id,
+                
+                url:cloudinaryResponseForAvatar.secure_url,
+            },
+            resume:{
+                public_id:cloudinaryResponseForAvatar.public_id,
+                
+                url:cloudinaryResponseForAvatar.secure_url,
+            },
+        });
+        res.status(200).json({
+            data:user,
+            success:true,
+            message:"user registered",
+        });
+    } catch (error) {
+        console.log("Error while creating user", error);
+    }
 
 });
