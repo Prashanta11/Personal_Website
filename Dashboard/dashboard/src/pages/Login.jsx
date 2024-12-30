5dd668fde467e87390a23ad8708604be7ea2da99
Prashanta Deuja <prashantadeuja@gmail.com>
2024-12-30 19:39:16 +0545

    design: design login page

--- Dashboard/dashboard/src/pages/Login.jsx
+++ Dashboard/dashboard/src/pages/Login.jsx
@@ -9,9 +9,9 @@ export function Login({
   ...props
 }) {
   return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Card className="overflow-hidden">
-        <CardContent className="grid p-0 md:grid-cols-2">
+    <div className={cn("flex flex-col items-center justify-center min-h-screen w-2/6  mx-auto", className)} {...props}>
+      <Card className="overflow-hidden w-full">
+        <CardContent className="grid p-0 md:grid-cols-1">
           <form className="p-6 md:p-8">
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
@@ -44,7 +44,7 @@ export function Login({
               <Button type="submit" className="w-full">
                 Login
               </Button>
-              <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+              <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center ">
                 <span className="relative z-10 bg-background px-2 text-muted-foreground">
                   Or continue with
                 </span>
@@ -86,20 +86,14 @@ export function Login({
               </div>
             </div>
           </form>
-          <div className="relative hidden bg-muted md:block">
-            <img
-              src="/placeholder.svg"
-              alt="Image"
-              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-            />
-          </div>
         </CardContent>
       </Card>
-      <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
+      <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary pt-4">
         By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
         and <a href="#">Privacy Policy</a>.
       </div>
     </div>
   )
 }
+
 export default Login;
