1ee922d54247fe2ca38d6bc7e52b8ee4c1cf132f
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-01 15:27:06 +0545

    fix: Ensure getUser action is dispatched on App mount

--- Dashboard/dashboard/src/App.jsx
+++ Dashboard/dashboard/src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -11,8 +13,14 @@ import ManageTimeline from "./pages/ManageTimeline";
 import ResetPassword from "./pages/ResetPassword";
 import UpdateProject from "./pages/UpdateProject";
 import ViewProject from "./pages/ViewProject";
+import { getUser } from "./store/slices/userSlice";
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUser());
+  },[]);
+
   return (
     <Router>
       <Routes>
