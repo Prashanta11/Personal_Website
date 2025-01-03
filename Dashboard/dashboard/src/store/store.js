630a6fbd0a7704c7954f9c770214077d0cc6dd62
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-03 23:05:32 +0545

    feat: Add messageSlice reducer to store for message management

--- Dashboard/dashboard/src/store/store.js
+++ Dashboard/dashboard/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import forgotPasswordReducer from './slices/forgotResetPasswordSlice';
+import messageSliceReducer from './slices/messageSlice';
 import userReducer from './slices/userSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     forgotPassword: forgotPasswordReducer,
+    messages: messageSliceReducer,
   },
 });
 
