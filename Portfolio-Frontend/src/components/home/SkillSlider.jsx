f1b35a3702668bf166de9b7d6f9ce53514f721c0
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-08 23:32:10 +0545

    feat: Add SkillSlider component for showcasing skills with a responsive carousel

---
+++ Portfolio-Frontend/src/components/home/SkillSlider.jsx
@@ -0,0 +1,34 @@
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+import { AutoScroll } from "@splidejs/splide-extension-auto-scroll";
+
+const SkillSlider = () => {
+  return (
+    <div className="py-10">
+      <Splide
+        options={{
+          type: "loop",
+          perPage: 6,
+          perMove: 1,
+          gap: "1rem",
+          autoScroll: {
+            speed: 1,
+          },
+          autoplay: false,
+          pauseOnHover: false,
+          arrows: false,
+          drag: "free",
+        }}
+        extensions={{ AutoScroll }}
+      >
+        <SplideSlide>
+          <div className="flex justify-center items-center border-2 border-gray-600 rounded-xl overflow-hidden">
+            <div className="h-20" />
+          </div>
+        </SplideSlide>
+      </Splide>
+    </div>
+  );
+};
+
+export default SkillSlider;
