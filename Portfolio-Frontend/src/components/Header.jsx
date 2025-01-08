4c5e9223bfce9475a5866d406f1fdb80c3f07de7
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-08 15:46:16 +0545

    feat: Refactor Header component to use dynamic navigation items

--- Portfolio-Frontend/src/components/Header.jsx
+++ Portfolio-Frontend/src/components/Header.jsx
@@ -2,36 +2,49 @@ import { NavLink } from 'react-router'
 import logo from '../assets/logo.png'
 
 const Header = () => {
+  const navItems = [{
+    name: 'Home',
+    link: '/'
+  },
+  {
+    name: 'About',
+    link: '/About'
+  },
+  {
+    name: 'Timeline',
+    link: '/Timeline'
+  },
+  {
+    name: 'Experience',
+    link: '/Experience'
+  },
+  {
+    name: 'Project',
+    link: '/Project'
+  },
+  {
+    name: 'Application',
+    link: '/Application'
+  },
+  {
+    name: 'Contact',
+    link: '/Contact'
+  },
+
+]
   return (
-    <div className="flex items-center justify-around  pl-5 py-2 bg-stone-400 shadow-xl shadow-stone-300/50  ">
+    <div className="flex items-center justify-between  p-5 py-2 bg-custom-gray shadow-xl shadow-stone-300/50  ">
       <div><img src={logo} alt="Logo"
       className='size-20 rounded-full' />
       </div>
 
-      <nav className='pl-5 '>
-        <ul className="flex space-x-4">
-          <li>
-            <NavLink to="/" className="hover:text-blue-500">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/About" className="hover:text-blue-500">About</NavLink>
-          </li>
-          <li>
-            <NavLink to="/Timeline" className="hover:text-blue-500">Timeline</NavLink>
-          </li>
-          <li>
-            <NavLink to="/Experience" className="hover:text-blue-500">Experience</NavLink>
-          </li>
-          <li>
-            <NavLink to="/Project" className="hover:text-blue-500">Project</NavLink>
-          </li>
-          <li>
-            <NavLink to="/Application" className="hover:text-blue-500">Apps</NavLink>
-          </li>
-          <li>
-            <NavLink to="/Contact" className="hover:text-blue-500">Contacts</NavLink>
-          </li>
-
+      <nav className='mx-auto '>
+        <ul className="flex space-x-6 text-lg font-semibold">
+          {navItems.map((item, index) => (
+            <li key={index}>
+              <NavLink to={item.link} className='text-stone-1000 hover:text-blue-800'>{item.name}</NavLink>
+            </li>
+          ))}
         </ul>
       </nav>
     </div>
