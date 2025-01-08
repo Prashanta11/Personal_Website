49124fec5fa514a3b979651927e3e4b2434d2d82
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-08 23:31:35 +0545

    feat: Refactor Header component for improved styling and navigation item formatting

--- Portfolio-Frontend/src/components/Header.jsx
+++ Portfolio-Frontend/src/components/Header.jsx
@@ -1,54 +1,59 @@
-import { NavLink } from 'react-router'
-import logo from '../assets/logo.png'
+import { NavLink } from "react-router";
+import logo from "../assets/logo.png";
 
 const Header = () => {
-  const navItems = [{
-    name: 'Home',
-    link: '/'
-  },
-  {
-    name: 'About',
-    link: '/About'
-  },
-  {
-    name: 'Timeline',
-    link: '/Timeline'
-  },
-  {
-    name: 'Experience',
-    link: '/Experience'
-  },
-  {
-    name: 'Project',
-    link: '/Project'
-  },
-  {
-    name: 'Application',
-    link: '/Application'
-  },
-  {
-    name: 'Contact',
-    link: '/Contact'
-  },
-
-]
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name: "About",
+      link: "/About",
+    },
+    {
+      name: "Timeline",
+      link: "/Timeline",
+    },
+    {
+      name: "Experience",
+      link: "/Experience",
+    },
+    {
+      name: "Project",
+      link: "/Project",
+    },
+    {
+      name: "Application",
+      link: "/Application",
+    },
+    {
+      name: "Contact",
+      link: "/Contact",
+    },
+  ];
   return (
-    <div className="flex items-center justify-between  p-5 py-2 bg-custom-gray shadow-xl shadow-stone-300/50  ">
-      <div><img src={logo} alt="Logo"
-      className='size-20 rounded-full' />
+    <div className="flex justify-between items-center bg-black shadow-stone-300/50 shadow-xl py-2 p-5">
+      <div>
+        <img src={logo} alt="Logo" className="rounded-full size-20" />
       </div>
 
-      <nav className='mx-auto '>
-        <ul className="flex space-x-6 text-lg font-semibold">
+      <nav className="mx-auto">
+        <ul className="flex space-x-6 font-semibold text-lg">
           {navItems.map((item, index) => (
             <li key={index}>
-              <NavLink to={item.link} className='text-stone-1000 hover:text-blue-800'>{item.name}</NavLink>
+              <NavLink
+                to={item.link}
+                className="text-gray-200 hover:text-gray-600"
+              >
+                {item.name}
+              </NavLink>
             </li>
           ))}
         </ul>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
