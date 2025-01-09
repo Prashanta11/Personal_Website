351be1329680fafbcb9a673e3a4c8380ea861919
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-10 00:24:16 +0545

    feat: Add MyApps component to showcase applications on the Home page

---
+++ Portfolio-Frontend/src/components/home/MyApps.jsx
@@ -0,0 +1,26 @@
+const MyApps = () => {
+  return (
+    <section className="border-2 border-gray-300 shadow-gray-400 shadow-sm mx-10 mt-32 px-4 py-3 rounded-xl">
+      <div className="mx-auto px-4 container">
+        <h2 className="mb-8 font-bold font-instrumentSans text-6xl text-center italic tracking-widest">
+          My Apps
+        </h2>
+        <div className="flex flex-wrap justify-center gap-4">
+          {Array.from({ length: 30 }).map((_, index) => (
+            <div
+              key={index}
+              className="bg-black rounded-lg overflow-hidden size-20"
+            >
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpuYdLEzBvwemix8pwsncUkLLOQqnByncadg&s"
+                className="w-full h-full"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MyApps;
