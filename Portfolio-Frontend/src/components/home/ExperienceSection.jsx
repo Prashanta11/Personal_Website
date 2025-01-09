a7e782deaab749c161b1c11d35739a7fc41b5938
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-10 00:23:47 +0545

    fix: Update ExperienceSection styles for improved visibility and consistency

--- Portfolio-Frontend/src/components/home/ExperienceSection.jsx
+++ Portfolio-Frontend/src/components/home/ExperienceSection.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const experiences = [
   {
     role: "Computer Teacher",
@@ -18,24 +16,21 @@ const experiences = [
 
 const ExperienceSection = () => {
   return (
-    <section className="border-2 border-gray-600 shadow-gray-400 shadow-sm mx-10 mt-32 px-4 py-3 rounded-xl">
+    <section className="border-2 border-gray-300 shadow-gray-400 shadow-sm mx-10 mt-32 px-4 py-3 rounded-xl">
       <div className="mx-auto px-4 container">
-        <h2 className="mb-8 font-bold font-instrumentSans text-6xl text-center text-gray-300 italic tracking-widest">
+        <h2 className="mb-8 font-bold font-instrumentSans text-6xl text-center italic tracking-widest">
           Experience
         </h2>
         <div className="gap-8 grid sm:grid-cols-1 md:grid-cols-2">
           {experiences.map((exp, index) => (
-            <div
-              key={index}
-              className="bg-custom-gray shadow-md hover:shadow-lg p-6 rounded-lg transition-shadow duration-300"
-            >
-              <h3 className="font-light text-2xl text-gray-300 italic tracking-widest">
+            <div key={index} className="bg-cardColor p-6 rounded-lg">
+              <h3 className="font-light text-2xl italic tracking-widest">
                 {exp.role}
               </h3>
               <div className="font-light text-xl italic">
                 <p className="font-medium text-gray-400">{exp.company}</p>
-                <p className="text-gray-300 text-sm">{exp.duration}</p>
-                <p className="mt-4 text-gray-300">{exp.description}</p>
+                <p className="text-sm">{exp.duration}</p>
+                <p className="mt-4">{exp.description}</p>
               </div>
             </div>
           ))}
