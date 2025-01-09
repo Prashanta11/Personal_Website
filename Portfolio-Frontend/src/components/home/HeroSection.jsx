e0c038caae906cbb6bf5c88ac8a28629b93e0515
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-10 00:24:06 +0545

    feat: Refactor HeroSection layout and styles for improved design and add MyApps component to Home page

--- Portfolio-Frontend/src/components/home/HeroSection.jsx
+++ Portfolio-Frontend/src/components/home/HeroSection.jsx
@@ -1,39 +1,23 @@
-import background from "../../assets/background.png";
-
 const HeroSection = () => {
   return (
-    <section className="py-20 text-white">
-      <div className="grid md:grid-cols-2 mx-auto px-6 container">
+    <section className="flex justify-center items-center h-full text-black">
+      <div className="grid md:grid-cols-1 mx-auto px-6 container">
         {/* Left Content - Text and Resume Button */}
         <div className="flex flex-col flex-1 justify-center items-center px-7 h-full text-center md:text-left">
           <h1 className="font-bold font-instrumentSans text-6xl text-center md:text-6xl italic leading-tight tracking-widest">
             Hi, I'm Prashanta
           </h1>
-          <p className="mt-4 max-w-lg font-light text-sm text-white md:text-xl italic -white">
+          <p className="mt-4 max-w-lg font-semibold text-black text-sm md:text-xl italic -black">
             A passionate Full-Stack Developer and Cybersecurity Enthusiast
             dedicated to building scalable, secure, and user-friendly solutions.
           </p>
           <a
             href="/resume.pdf" // Replace with the actual resume file path
             download
-            className="inline-block bg-slate-200 hover:bg-slate-50 shadow-sm shadow-white mt-8 px-8 py-3 rounded font-semibold text-black"
+            className="inline-block bg-slate-200 hover:bg-slate-50 shadow-black shadow-sm mt-8 px-8 py-3 rounded font-semibold text-black"
           >
             Download Resume
           </a>
-        </div>
-
-        {/* Right Content - Image */}
-        <div className="flex-1 mt-10 md:mt-0">
-          <div className="relative overflow-hidden">
-            {/* Background Shape */}
-            <div className="absolute inset-0 bg-black opacity-30 blur-3xl rounded-full transform scale-125"></div>
-            {/* Main Image */}
-            <img
-              src={background} //  image path
-              alt="Prashanta"
-              className="mx-auto rounded-lg w-full max-w-md md:max-w-sm"
-            />
-          </div>
         </div>
       </div>
     </section>
--- Portfolio-Frontend/src/pages/Home.jsx
+++ Portfolio-Frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import AboutSection from "@/components/home/AboutSection";
 import ExperienceSection from "@/components/home/ExperienceSection";
 import HeroSection from "@/components/home/HeroSection";
+import MyApps from "@/components/home/MyApps";
 import Projects from "@/components/home/Projects";
 import SkillSlider from "@/components/home/SkillSlider";
 import Timeline from "@/components/home/Timeline";
@@ -9,14 +10,25 @@ import Timeline from "@/components/home/Timeline";
 const Home = () => {
   return (
     <>
-      <Header />
-      <div className="bg-black text-white">
-        <HeroSection />
-        <SkillSlider />
-        <AboutSection />
-        <Timeline />
-        <Projects />
-        <ExperienceSection />
+      <div className="bg-bodyColor pb-10 text-black">
+        <div
+          style={{
+            backgroundImage: "url('https://www.picsum.photos/1920/1080')",
+          }}
+          className="relative bg-cover bg-no-repeat bg-center h-screen text-black"
+        >
+          <Header />
+          <HeroSection />
+          <div className="absolute inset-0 bg-black/10 pointer-events-none"></div>
+        </div>
+        <div className="px-10">
+          <SkillSlider />
+          <AboutSection />
+          <Timeline />
+          <Projects />
+          <ExperienceSection />
+          <MyApps />
+        </div>
       </div>
     </>
   );
