d293e9f9c7f87a86cdd731e8de7d4dd0cd63d951
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-11 21:34:29 +0545

    fix: Update background color in Footer component for improved contrast

--- Portfolio-Frontend/src/components/Footer.jsx
+++ Portfolio-Frontend/src/components/Footer.jsx
@@ -58,7 +58,7 @@ const Footer = () => {
                 type="text"
                 id="subject"
                 name="subject"
-                className="border-gray-100 bg-gray-600 px-3 py-2 border rounded focus:ring-2 focus:ring-blue-500 w-full text-white focus:outline-none"
+                className="border-gray-100 bg-gray-300 px-3 py-2 border rounded focus:ring-2 focus:ring-blue-500 w-full text-white focus:outline-none"
                 required
               />
             </div>
