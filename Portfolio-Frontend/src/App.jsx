7090d4dc7df38b1d276ac18c7ab943dc32f572a2
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-12 23:48:57 +0545

    feat: Add About, Timeline, and Projects routes to the application

--- Portfolio-Frontend/src/App.jsx
+++ Portfolio-Frontend/src/App.jsx
@@ -1,18 +1,28 @@
-
-import { BrowserRouter, Route, Routes } from "react-router"
-import './App.css'
-import Home from "./pages/Home"
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./App.css";
+import Home from "./pages/Home";
+import AboutSection from "./components/home/AboutSection";
+import Timeline from "./components/home/Timeline";
+import Projects from "./components/home/Projects";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route
+          path="/about"
+          element={
+            <AboutSection className="flex justify-center items-center h-screen" />
+          }
+        />
+        <Route path="/timeline" element={<Timeline />} />
+        <Route path="/project" element={<Projects />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
-
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
