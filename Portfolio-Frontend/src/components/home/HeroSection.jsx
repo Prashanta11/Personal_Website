42052cee21a2f77bd8743bb9c7ba4c614bf286d2
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-12 23:49:50 +0545

    feat: Enhance HeroSection with updated content and social media links

--- Portfolio-Frontend/src/components/home/HeroSection.jsx
+++ Portfolio-Frontend/src/components/home/HeroSection.jsx
@@ -1,3 +1,7 @@
+import { FaLinkedin } from "react-icons/fa";
+import { FaSquareGithub } from "react-icons/fa6";
+import { BiLogoGmail } from "react-icons/bi";
+
 const HeroSection = () => {
   return (
     <section className="flex justify-center items-center h-full text-black">
@@ -7,9 +11,10 @@ const HeroSection = () => {
           <h1 className="font-bold font-instrumentSans text-6xl text-center md:text-6xl italic leading-tight tracking-widest">
             Hi, I'm Prashanta
           </h1>
-          <p className="mt-4 max-w-lg font-semibold text-black text-sm md:text-xl italic -black">
-            A passionate Full-Stack Developer and Cybersecurity Enthusiast
-            dedicated to building scalable, secure, and user-friendly solutions.
+          <p className="mt-4 max-w-lg font-semibold text-black text-sm md:text-2xl italic -black">
+            —a curious mind crafting code by day and melodies by night. Whether
+            coding solutions or singing tunes, I’m all about turning passion
+            into joy and exploring what’s next!
           </p>
           <a
             href="/resume.pdf" // Replace with the actual resume file path
@@ -18,6 +23,33 @@ const HeroSection = () => {
           >
             Download Resume
           </a>
+
+          <div className="flex gap-5 mt-4">
+            <a
+              href=""
+              target="_blank"
+              rel="noreferrer"
+              className="flex items-center mt-4 font-semibold text-black text-xl hover:text-blue-800"
+            >
+              <FaSquareGithub size={30} />
+            </a>
+            <a
+              href="https://www.facebook.com/prashanta.kc.1"
+              target="_blank"
+              rel="noreferrer"
+              className="flex items-center mt-4 font-semibold text-black text-xl hover:text-blue-800"
+            >
+              <FaLinkedin size={30} />
+            </a>
+            <a
+              href=""
+              target="_blank"
+              rel="noreferrer"
+              className="flex items-center mt-4 font-semibold text-black text-xl hover:text-blue-800"
+            >
+              <BiLogoGmail size={30} />
+            </a>
+          </div>
         </div>
       </div>
     </section>
