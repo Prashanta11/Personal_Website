5151404dc7d3009627bc6be4c69de7c402256d35
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-14 00:36:24 +0545

    feat: Enhance ExperienceSection styling and improve experience description display

--- Portfolio-Frontend/src/components/home/ExperienceSection.jsx
+++ Portfolio-Frontend/src/components/home/ExperienceSection.jsx
@@ -25,7 +25,7 @@ const ExperienceSection = () => {
   console.log(experiencesData);
 
   return (
-    <section className="border-2 border-gray-300 shadow-gray-400 shadow-sm mx-10 mt-32 px-4 py-3 rounded-xl">
+    <section className="border-2 border-gray-300 bg-gray-200 hover:bg-bodyColor shadow-gray-400 shadow-sm mt-32 px-4 py-3 rounded-xl">
       <div className="mx-auto px-4 container">
         <h2 className="mb-8 font-bold font-instrumentSans text-6xl text-center italic tracking-widest">
           Experience
@@ -33,12 +33,14 @@ const ExperienceSection = () => {
         <div className="gap-8 grid sm:grid-cols-1 md:grid-cols-2">
           {experiencesData?.length > 0 &&
             experiencesData.map((exp, index) => (
-              <div key={index} className="bg-cardColor p-6 rounded-lg">
-                <h3 className="font-light text-2xl italic tracking-widest">
+              <div key={index} className="bg-cardColor my-2 p-6 rounded-lg">
+                <h3 className="pb-2 font-light text-2xl italic tracking-widest">
                   {exp.title}
                 </h3>
                 <div className="font-light text-xl italic">
-                  <p className="font-medium text-gray-400">ABC </p>
+                  <p className="font-light text-gray-800">
+                    {exp?.description}{" "}
+                  </p>
                   <p className="text-sm">{exp.duration}</p>
                   <p className="mt-4">
                     {exp?.date?.from} - {exp?.date?.to}
