4d1bc9de58ed8aa39aa8edbd3750acd97b23d785
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-14 18:30:08 +0545

    feat: Update CSS for consistent box-sizing and modify API base URL for dynamic host resolution

--- Portfolio-Frontend/src/api/api.js
+++ Portfolio-Frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:5000/api/v1";
+const baseURL = `http://${window.location.hostname}:5000/api/v1`;
 
 const get = async (url, options) => {
   const response = await axios.get(`${baseURL}/${url}`, options);
