838d934ab2509ecd1057e49573ebd207beed00ce
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-14 18:32:09 +0545

    feat: Add id attribute to ExperienceSection for improved navigation and accessibility

--- Portfolio-Frontend/src/components/home/ExperienceSection.jsx
+++ Portfolio-Frontend/src/components/home/ExperienceSection.jsx
@@ -25,14 +25,17 @@ const ExperienceSection = () => {
   console.log(experiencesData);
 
   return (
-    <section className="border-2 border-gray-300 bg-gray-200 hover:bg-bodyColor shadow-gray-400 shadow-sm mt-32 px-4 py-3 rounded-xl">
+    <section
+      id="experience"
+      className="border-2 border-gray-300 bg-gray-200 hover:bg-bodyColor shadow-gray-400 shadow-sm mt-32 px-4 py-3 rounded-xl"
+    >
       <div className="mx-auto px-4 container">
         <h2 className="mb-8 font-bold font-instrumentSans text-6xl text-center italic tracking-widest">
           Experience
         </h2>
         <div className="gap-8 grid sm:grid-cols-1 md:grid-cols-2">
           {experiencesData?.length > 0 &&
-            experiencesData.map((exp, index) => (
+            experiencesData?.map((exp, index) => (
               <div key={index} className="bg-cardColor my-2 p-6 rounded-lg">
                 <h3 className="pb-2 font-light text-2xl italic tracking-widest">
                   {exp.title}
