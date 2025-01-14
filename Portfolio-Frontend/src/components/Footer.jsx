ad3f19cd98dc693dd56a2fd31fbeb0b6431b817e
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-14 18:30:37 +0545

    feat: Enhance Footer component layout and styling for better structure and responsiveness

--- Portfolio-Frontend/src/components/Footer.jsx
+++ Portfolio-Frontend/src/components/Footer.jsx
@@ -31,109 +31,117 @@ const Footer = () => {
   }, [message]);
 
   return (
-    <footer className="border-gray-300 bg-gray-200 hover:bg-bodyColor shadow-gray-400 shadow-sm mt-20 px-4 py-10 rounded-xl text-black">
-      <div className="flex md:flex-row flex-col justify-between gap-8 mx-auto container">
-        {/* About Section */}
-        <div className="flex-1">
-          <h3 className="mb-2 font-bold font-instrumentSans text-6xl italic tracking-widest">
-            Let's Create Together
-          </h3>
-          <p className="mb-4 py-7 font-semibold text-2xl italic">
-            Thank you for visiting! Curious to learn more about my journey and
-            approach? I’d love to share over a coffee chat or during an
-            interview. Feel free to connect—I’m just a message away!
-          </p>
-          <div className="flex gap-5 mt-4">
-            <a
-              href=""
-              target="_blank"
-              rel="noreferrer"
-              className="flex items-center mt-4 font-semibold text-black text-xl hover:text-blue-800"
+    <>
+      <footer className="border-gray-300 bg-gray-200 hover:bg-bodyColor shadow-gray-400 shadow-sm mt-20 px-4 py-10 rounded-xl text-black">
+        <div className="flex md:flex-row flex-col justify-between gap-8 mx-auto container">
+          {/* About Section */}
+          <div className="flex flex-col flex-1 justify-between text-center">
+            <h3 className="mb-2 font-bold font-instrumentSans text-6xl italic tracking-widest">
+              Let's Create Together
+            </h3>
+            <p className="mb-4 py-7 font-semibold text-2xl italic">
+              Thank you for visiting! Curious to learn more about my journey and
+              approach? I’d love to share over a coffee chat or during an
+              interview. Feel free to connect—I’m just a message away!
+            </p>
+            <div className="flex justify-center gap-5 mt-4">
+              <a
+                href=""
+                target="_blank"
+                rel="noreferrer"
+                className="flex items-center mt-4 font-semibold text-black text-xl hover:text-blue-800"
+              >
+                <BiLogoGmail size={30} />
+              </a>
+              <a
+                href="https://www.facebook.com/prashanta.kc.1"
+                target="_blank"
+                rel="noreferrer"
+                className="flex items-center mt-4 font-semibold text-black text-xl hover:text-blue-800"
+              >
+                <FaLinkedin size={30} />
+              </a>
+              <a
+                href=""
+                target="_blank"
+                rel="noreferrer"
+                className="flex items-center mt-4 font-semibold text-black text-xl hover:text-blue-800"
+              >
+                <FaSquareGithub size={30} />
+              </a>
+            </div>
+          </div>
+
+          {/* Contact Form Section */}
+          <div
+            id="contact"
+            className="flex-1 border-2 border-gray-300 shadow-gray-400 shadow-sm md:mx-10 px-7 py-7 rounded-xl"
+          >
+            <h3 className="mb-2 font-bold text-2xl">Contact Me</h3>
+            <form
+              onSubmit={handleSubmit}
+              action={handleSubmit}
+              // method="POST"
+              className="space-y-4"
             >
-              <BiLogoGmail size={30} />
-            </a>
-            <a
-              href="https://www.facebook.com/prashanta.kc.1"
-              target="_blank"
-              rel="noreferrer"
-              className="flex items-center mt-4 font-semibold text-black text-xl hover:text-blue-800"
-            >
-              <FaLinkedin size={30} />
-            </a>
-            <a
-              href=""
-              target="_blank"
-              rel="noreferrer"
-              className="flex items-center mt-4 font-semibold text-black text-xl hover:text-blue-800"
-            >
-              <FaSquareGithub size={30} />
-            </a>
+              <div>
+                <label className="block mb-1" htmlFor="name">
+                  Your Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="senderName"
+                  className="border-gray-100 bg-gray-300 px-3 py-2 border rounded focus:ring-2 focus:ring-blue-500 w-full text-black focus:outline-none"
+                  required
+                />
+              </div>
+              <div>
+                <label className="block mb-1" htmlFor="subject">
+                  Subject
+                </label>
+                <input
+                  type="text"
+                  id="subject"
+                  name="subject"
+                  className="border-gray-100 bg-gray-300 px-3 py-2 border rounded focus:ring-2 focus:ring-blue-500 w-full text-black focus:outline-none"
+                  required
+                />
+              </div>
+              <div>
+                <label className="block mb-1" htmlFor="message">
+                  Message
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  rows="4"
+                  className="border-gray-100 bg-gray-300 px-3 py-2 border rounded focus:ring-2 focus:ring-blue-500 w-full text-black focus:outline-none"
+                  required
+                ></textarea>
+              </div>
+              <button
+                type="submit"
+                disabled={loading}
+                className="inline-flex justify-center items-center gap-1 bg-slate-200 hover:bg-slate-50 shadow-black shadow-sm mt-8 px-8 py-3 rounded font-semibold text-black"
+              >
+                Send Message{" "}
+                {loading && <Loader2 size={20} className="animate-spin" />}
+              </button>
+              {message && (
+                <p className="flex gap-1 text-green-400 text-sm">
+                  <Check size={20} />
+                  {message}
+                </p>
+              )}
+            </form>
           </div>
         </div>
-
-        {/* Contact Form Section */}
-        <div className="flex-1 border-2 border-gray-300 shadow-gray-400 shadow-sm mx-10 px-7 py-7 rounded-xl">
-          <h3 className="mb-2 font-bold text-2xl">Contact Me</h3>
-          <form
-            onSubmit={handleSubmit}
-            action={handleSubmit}
-            // method="POST"
-            className="space-y-4"
-          >
-            <div>
-              <label className="block mb-1" htmlFor="name">
-                Your Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="senderName"
-                className="border-gray-100 bg-gray-300 px-3 py-2 border rounded focus:ring-2 focus:ring-blue-500 w-full text-white focus:outline-none"
-                required
-              />
-            </div>
-            <div>
-              <label className="block mb-1" htmlFor="subject">
-                Subject
-              </label>
-              <input
-                type="text"
-                id="subject"
-                name="subject"
-                className="border-gray-100 bg-gray-300 px-3 py-2 border rounded focus:ring-2 focus:ring-blue-500 w-full text-white focus:outline-none"
-                required
-              />
-            </div>
-            <div>
-              <label className="block mb-1" htmlFor="message">
-                Message
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                rows="4"
-                className="border-gray-100 bg-gray-300 px-3 py-2 border rounded focus:ring-2 focus:ring-blue-500 w-full text-white focus:outline-none"
-                required
-              ></textarea>
-            </div>
-            <button
-              type="submit"
-              disabled={loading}
-              className="inline-flex justify-center items-center gap-1 bg-slate-200 hover:bg-slate-50 shadow-black shadow-sm mt-8 px-8 py-3 rounded font-semibold text-black"
-            >
-              Send Message{" "}
-              {loading && <Loader2 size={20} className="animate-spin" />}
-            </button>
-            {message && (
-              <p className="flex gap-1 text-green-400 text-sm">
-                <Check size={20} />
-                {message}
-              </p>
-            )}
-          </form>
-        </div>
+      </footer>
+      <div className="pt-10 w-full text-center">
+        Copyright &copy; 2021, Prashanta Deuja
       </div>
-    </footer>
+    </>
   );
 };
 
