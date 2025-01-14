1bd366e502a1cf668f7cb63dd6c5778ba4a0f99d
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-14 19:18:54 +0545

    refactor: Improve code formatting and update dotenv path in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,23 +11,25 @@ import skillRouter from "./routes/skillRoutes.js";
 import applicationRouter from "./routes/softwareApplicationRoutes.js";
 import timelineRouter from "./routes/timelineRoutes.js";
 import userRouter from "./routes/userRoutes.js";
-const app=express();
-dotenv.config({path:"./.env"})
+const app = express();
+dotenv.config({ path: "../.env" });
 
-app.use(cors({
-    origin:[process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL],
-    methods:["GET", "POST", "DELETE", "PUT"],
-    credentials:true,
-})
+app.use(
+  cors({
+    origin: [process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
 );
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(fileUpload({
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  fileUpload({
     useTempFiles: true,
     tempFileDir: "/tmp/",
-})
+  })
 );
 
 app.use("/api/v1/message", messageRoutes);
@@ -35,11 +37,9 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/timeline", timelineRouter);
 app.use("/api/v1/experience", experienceRouter);
 app.use("/api/v1/softwareApplication", applicationRouter);
-app.use("/api/v1/skill",skillRouter );
-app.use("/api/v1/project",projectRouter );
+app.use("/api/v1/skill", skillRouter);
+app.use("/api/v1/project", projectRouter);
 
 app.use(errorMiddleware);
 
 export default app;
-
-
