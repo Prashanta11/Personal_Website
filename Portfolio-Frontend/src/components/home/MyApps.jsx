e52f8aecf6671c669a8216bff29cc9ef710ecb1c
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-14 18:32:17 +0545

    feat: Add id attribute to MyApps section for improved navigation

--- Portfolio-Frontend/src/components/home/MyApps.jsx
+++ Portfolio-Frontend/src/components/home/MyApps.jsx
@@ -10,13 +10,16 @@ const MyApps = () => {
   }, []);
 
   return (
-    <section className="border-2 border-gray-300 bg-gray-200 hover:bg-bodyColor shadow-gray-400 shadow-sm mt-32 px-4 py-3 rounded-xl">
+    <section
+      id="application"
+      className="border-2 border-gray-300 bg-gray-200 hover:bg-bodyColor shadow-gray-400 shadow-sm mt-32 px-4 py-3 rounded-xl"
+    >
       <div className="mx-auto px-4 container">
         <h2 className="mb-14 font-bold font-instrumentSans text-6xl text-center italic tracking-widest">
           My Apps
         </h2>
         <div className="flex flex-wrap justify-center gap-20 pb-8">
-          {apps.map((app, index) => (
+          {apps?.map((app, index) => (
             <div
               key={index}
               className="hover:shadow-lg rounded-lg transform transition-transform overflow-hidden hover:scale-105 size-20"
