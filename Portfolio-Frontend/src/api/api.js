c7e392b3e8ede55c3f9cc496a221166b955acd1f
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-14 22:25:51 +0545

    fix: Update API base URL to new deployment

--- Portfolio-Frontend/src/api/api.js
+++ Portfolio-Frontend/src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = `https://personal-website-adx1eyp3n-prashanta11s-projects.vercel.app/api/v1`;
+const baseURL = `https://personal-website-nine-theta.vercel.app/api/v1`;
+// const baseURL = `https://personal-website-adx1eyp3n-prashanta11s-projects.vercel.app/api/v1`;
 
 const get = async (url, options) => {
   const response = await axios.get(`${baseURL}/${url}`, options);
