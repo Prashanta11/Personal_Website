7cfaaba8c396f6b3c9c18ed8b1dff5afc44cdb0c
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-16 22:31:30 +0545

    fix: Enhance JWT token generation with SameSite and secure cookie attributes

--- Backend/src/utils/jwtToken.js
+++ Backend/src/utils/jwtToken.js
@@ -1,15 +1,21 @@
-
-export const generateToken =(user, message, statuscode, res) =>{
-    const token = user.generateJsonWebToken();
-
-    res.status(statuscode).cookie("token",token,{
-        expires: new Date(Date.now()+ process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
-        httpOnly:true
-        
-    }).json({
-        success: true,
-        message,
-        user,
-        token,
+export const generateToken = (user, message, statuscode, res, req = null) => {
+  const token = user.generateJsonWebToken();
+  //   const domain = req?.headers?.host?.split(":")?.[0];
+  res
+    .status(statuscode)
+    .cookie("token", token, {
+      expires: new Date(
+        Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true,
+      path: "/",
+      sameSite: "None", // Set SameSite attribute to "Strict", "Lax", or "None"
+      secure: process.env.NODE_ENV === "production", // Set secure flag if in production,
     })
+    .json({
+      success: true,
+      message,
+      user,
+      token,
+    });
 };
