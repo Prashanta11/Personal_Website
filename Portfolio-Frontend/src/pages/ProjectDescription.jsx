729dbba6a82825905a611432ef3d5ce25ff82964
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-19 22:33:06 +0545

    fix: Update ProjectDescription component layout for improved readability and responsiveness

--- Portfolio-Frontend/src/pages/ProjectDescription.jsx
+++ Portfolio-Frontend/src/pages/ProjectDescription.jsx
@@ -18,39 +18,41 @@ const ProjectDescription = () => {
   return (
     <div className="md:px-10 pt-6">
       <div className="border-2 border-gray-300 bg-gray-200 hover:bg-bodyColor shadow-gray-400 shadow-sm mx-2 md:mx-10 px-4 md:px-7 py-6 rounded-xl transition-colors">
-        <h2 className="pb-7 font-bold font-instrumentSans text-6xl text-center italic capitalize tracking-widest">
+        <h2 className="pb-7 font-bold font-instrumentSans text-3xl text-center md:text-6xl italic capitalize tracking-widest">
           {projectDetail?.title}
         </h2>
 
         <div className="flex flex-col gap-6 rounded-xl overflow-hidden group">
           <div className="flex justify-center items-center w-full">
             <img
-              className="w-full h-96 object-cover"
+              className="rounded-md w-full h-96 overflow-hidden object-contain"
               src={projectDetail?.projectBanner?.url}
               alt=""
             />
           </div>
           <div className="flex justify-center items-center gap-6">
-            <Link to={projectDetail?.gitRepoLink}>
+            <Link target="_blank" to={projectDetail?.gitRepoLink}>
               <FaGithub className="text-4xl hover:text-blue-600" />
             </Link>
-            <Link to={projectDetail?.projectLink}>
+            <Link target="_blank" to={projectDetail?.projectLink}>
               <ExternalLink className="inline-block font-medium text-6xl hover:text-blue-600" />
             </Link>
           </div>
-          <div className="space-y-3">
-            <div className="flex gap-3">
-              <p className="md:w-1/12 font-medium">Technologies:</p>
-              <p className="">{projectDetail?.technologies}</p>
+          <div className="border-gray-300 border rounded-lg">
+            <div className="space-y-3 mb-2 px-3 py-2">
+              <div className="flex gap-3">
+                <p className="md:w-1/12 font-medium">Technologies:</p>
+                <p className="">{projectDetail?.technologies}</p>
+              </div>
+              <div className="flex gap-3">
+                <p className="md:w-1/12 font-medium">Stack:</p>
+                <p className="">{projectDetail?.stack}</p>
+              </div>
             </div>
-            <div className="flex gap-3">
-              <p className="md:w-1/12 font-medium">Stack:</p>
-              <p className="">{projectDetail?.stack}</p>
-            </div>
+            <p className="border-gray-300 p-2 pt-4 border border-t w-full">
+              {projectDetail?.description}
+            </p>
           </div>
-          <p className="p-2 border rounded-md w-full">
-            {projectDetail?.description}
-          </p>
         </div>
       </div>
     </div>
