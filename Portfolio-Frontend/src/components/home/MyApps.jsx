cd967921ee0e66a484b2e5dea0b54541ac9b5c93
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-19 22:32:31 +0545

    fix: Refactor MyApps component layout for improved responsiveness and text visibility

--- Portfolio-Frontend/src/components/home/MyApps.jsx
+++ Portfolio-Frontend/src/components/home/MyApps.jsx
@@ -14,15 +14,15 @@ const MyApps = () => {
       id="application"
       className="border-2 border-gray-300 bg-gray-200 hover:bg-bodyColor shadow-gray-400 shadow-sm mt-6 sm:mt-24 px-4 py-3 rounded-xl"
     >
-      <div className="mx-auto px-4 container">
-        <h2 className="mb-14 font-bold font-instrumentSans text-6xl text-center italic tracking-widest">
+      <div className="flex flex-col gap-5 sm:gap-8 mx-auto px-4 container">
+        <h2 className="font-bold font-instrumentSans text-4xl text-center sm:text-6xl italic tracking-widest">
           My Apps
         </h2>
-        <div className="flex flex-wrap justify-center gap-20 pb-8">
+        <div className="sm:flex max-[500px]:flex-col sm:flex-wrap justify-center max-[500px]:items-center gap-8 sm:gap-20 grid grid-cols-2 sm:pb-8">
           {apps?.map((app, index) => (
             <div
               key={index}
-              className="hover:shadow-lg rounded-lg transform transition-transform overflow-hidden hover:scale-105 size-20"
+              className="hover:shadow-lg rounded-lg max-[500px]:w-full transform transition-transform overflow-hidden hover:scale-105 size-20"
             >
               <img src={app?.svg?.url} className="w-full h-full" />
             </div>
